ed7c5c37d8717440d2eb6e109f861563003bf64a
Jarosław Lubiński <jaroslaw.lubinski1@gmail.com>
2018-04-17 14:31:55 +0200

    MainWindow

---
+++ KMZ/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Win32;
+using SharpKml.Engine;
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace KMZ
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public KmzFile CurrentFile { get; set; }
+        public KmlFile CurrentKML { get; set; }
+
+        private void OnLoadFileClick(object sender, RoutedEventArgs e)
+        {
+            Stream str = null;
+            OpenFileDialog LoadWindow = new OpenFileDialog()
+            {
+                InitialDirectory = "c:\\Pulpit",
+                Filter = "KMZ (*.kmz)|*.kmz|All files (*.*)|*.*",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            };
+
+            try
+            {
+                LoadWindow.ShowDialog();
+                str = LoadWindow.OpenFile();
+                if (str != null)
+                {
+                    CurrentFile = KmzFile.Open(str);
+                    str.Dispose();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            foreach (var name in CurrentFile.Files)
+            {
+                Stack.Children.Add(new TextBlock()
+                {
+                    Text = name.ToString()
+                });
+            }
+
+            CurrentKML = CurrentFile.GetDefaultKmlFile();
+        }
+    }
+}
