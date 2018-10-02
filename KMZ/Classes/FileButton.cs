19b515c7b6f35cd4a86c5e3d94c7c43039477d94
Jarosław Lubiński <jaroslaw.lubinski1@gmail.com>
2018-10-02 16:36:44 +0200

    KMLButton changed into generic FileButton

---
+++ KMZ/Classes/FileButton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using SharpKml.Dom;
+using SharpKml.Engine;
+
+namespace KMZ.Classes
+{
+    public class FileButton<T> : Button
+    {
+        public FileButton() { }
+
+        public FileButton(T input)
+        {
+            File = input;
+        }
+
+        public T File { get; set; }
+        public bool IsClicked { get; set; }
+    }
+}
