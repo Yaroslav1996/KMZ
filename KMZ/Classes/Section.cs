e454cfcf880f4393ee46f35cf6a3512c6f9f05ab
Jarosław Lubiński <jaroslaw.lubinski1@gmail.com>
2018-10-02 16:37:36 +0200

    Section class created

---
+++ KMZ/Classes/Section.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMZ.Classes
+{
+    public class Section
+    {
+        public Section(string n, Image im)
+        {
+            Name = n;
+            Sec = im;
+        }
+
+        public String Name { get; set; }
+
+        public Image Sec { get; set; }
+    }
+}
