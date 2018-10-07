a7a2c0e3505cea7756d6c220fe93844bacdb1052
Jarosław Lubiński <jaroslaw.lubinski1@gmail.com>
2018-10-07 14:10:54 +0200

    Adding a window showing sections

---
+++ KMZ/Pages/SectionReadWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using KMZ.Classes;
+
+namespace KMZ.Pages
+{
+    /// <summary>
+    /// Interaction logic for SectionReadWindow.xaml
+    /// </summary>
+    public partial class SectionReadWindow : Window
+    {
+        public SectionReadWindow(Section section)
+        {
+            InitializeComponent();
+            SectionName.Text = section.Name;
+            SectionImage.Source = BitmapConversion.ToWpfBitmap(section.Image);
+        }
+
+       
+    }
+}
