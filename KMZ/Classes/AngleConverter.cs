6158befcb4e2d65c1958905e5184dd49747cd196
Jarosław Lubiński <jaroslaw.lubinski1@gmail.com>
2018-10-16 12:50:17 +0200

    Class converting angles

---
+++ KMZ/Classes/AngleConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace KMZ.Classes
+{
+    internal static class AngleConverter
+    {
+        public static double ConvertDegreesToRadians(double angle)
+        {
+            return Math.PI * angle / 180.0;
+        }
+
+        public static double ConvertRadiansToDegrees(double angle)
+        {
+            return 180.0 * angle / Math.PI;
+        }
+    }
+}
